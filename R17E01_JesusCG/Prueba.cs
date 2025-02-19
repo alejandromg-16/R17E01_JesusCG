70b894b5d783bfa8181a1f1c644bcff4330688b4
Alumno <Alumno@DAM1-PC9>
2025-02-19 09:07:46 +0100

    Estableciiento de la estructura del modelo de datos (clase instanciable)

--- R17E01_JesusCG/Prueba.cs
+++ R17E01_JesusCG/Prueba.cs
@@ -11,5 +11,28 @@ namespace R17E01_JesusCG
     /// </summary>
    public  class Prueba
     {
+        #region CONSTANTES - PARAMETROS DE CONFIGURACION DE LA CLASE 
+
+        #endregion
+
+        #region MIEMBROS | CAMPOS | ATRIBUTOS PRIVADOS
+        #endregion
+
+        #region CONSTRUCTORES 
+        #endregion
+        //Mecanismo de acceso a la informacion que mantiene la clase (datos)
+        //Proporcciona dos descriptores de acceso (get y set)
+        //Su mision es la de proteger los datos que entran a las clase (set)
+        //o se transfieren fuera de la clase (get)
+
+        #region PROPIEDADES
+        #endregion
+
+        #region METODOS PRIVADOS - INTERNO DE LA CLASE
+        #endregion
+
+        #region METODOS PUBLICOS - ACCESIBLES DESDE FUERA DE LA CLASE
+        #endregion
+
     }
 }
